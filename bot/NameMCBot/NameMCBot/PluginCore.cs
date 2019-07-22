c32bbafc106c0f034a5fb8437d485f79127db16a
Adibör <44465736+0Adiber@users.noreply.github.com>
2019-07-22 12:28:45 +0200

    first commit

---
+++ bot/NameMCBot/NameMCBot/PluginCore.cs
@@ -0,0 +1,53 @@
+﻿using NameMCBot.Tasks;
+using OQ.MineBot.PluginBase.Base;
+using OQ.MineBot.PluginBase.Base.Plugin;
+using OQ.MineBot.PluginBase.Bot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameMCBot
+{
+    [Plugin(1, "NameMCBot", "This Plugin is a NameMC Bot.", "adiber.at")]
+    public class Bot : IStartPlugin
+    {
+
+        // Must be overriden by every plugin.
+        public override void OnLoad(int version, int subversion, int buildversion)
+        {
+            //this.Setting.Add(new StringSetting("Authorised Users", "Users that are allowed to use this Plugin by the Chat command", "Adiber,MrHunh"));
+        }
+
+        public override PluginResponse OnEnable(IBotSettings botSettings)
+        {
+            // Called once the plugin is ticked in the plugin tab.
+            if (!botSettings.loadChat) return new PluginResponse(false, "'Load chat' must be enabled.");
+
+            return new PluginResponse(true);
+        }
+
+        public override void OnDisable()
+        {
+            // Called once the plugin is unticked.
+            // (Note: does not get called if the plugin is stopped from different sources, such as macros)
+            Console.WriteLine("Plugin disabled");
+        }
+
+        public override void OnStart()
+        {
+            // This should be used to register the tasks for the bot, see below.
+            // (Note: called after 'OnEnable')
+
+            RegisterTask(new Query());
+
+        }
+
+        public override void OnStop()
+        {
+            // Called once the plugin is stopped.
+            // (Note: unlike 'OnDisabled' this gets triggered from other sources, not only plugins tab)
+        }
+    }
+}
---
+++ bot/NameMCBot/NameMCBot/Tasks/Query.cs
@@ -0,0 +1,135 @@
+﻿using OQ.MineBot.PluginBase;
+using OQ.MineBot.PluginBase.Base.Plugin.Tasks;
+using OQ.MineBot.PluginBase.Classes.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NameMCBot.Tasks
+{
+    class Query : ITask
+    {
+        public Query()
+        {
+
+        }
+
+        public override bool Exec()
+        {
+            return true;
+        }
+
+        public override void Start()
+        {
+            player.events.onChat += OnChat;
+        }
+
+        public override void Stop()
+        {
+            player.events.onChat -= OnChat;
+        }
+
+        private void OnChat(IPlayer player, IChat message, byte position)
+        {
+            string msg = message.GetText();
+            
+            if(msg.StartsWith("[Clans]"))
+            {
+                if (!msg.Contains(":")) return;
+                string[] parts = msg.Split(new char[] { ':' });
+                if (!parts[1].Trim().StartsWith(".")) return;
+
+                string[] cmd = parts[1].Split(new char[] { ' ' });
+                if (!(cmd.Length != 2)) player.functions.Chat("/cc [NameMCBot] Command: '.nr <name>'");
+
+                string site = GetPageAsString("https://de.namemc.com/search?q=" + cmd[1]);
+
+                int start = site.IndexOf("py-0");
+                site = site.Substring(start, site.IndexOf("/main") - start);
+
+                //List<string> result = new List<string>();
+
+                player.functions.Chat("/cc [NameMCBot] ~-~-~-~-~ \"" + cmd[1] + "\"~-~-~-~-~");
+
+                foreach (string p in getHtmlSplitted(site))
+                {
+                    if (String.IsNullOrWhiteSpace(p)) break;
+                    //result.Add(p);
+                    player.functions.Chat("/cc - " + p);
+                }
+
+                player.functions.Chat("/cc [NameMCBot] ~-~-~-~-~ENDE~-~-~-~-~");
+
+            }
+        }
+
+        private static string GetPageAsString(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
+
+            var content = String.Empty;
+            HttpStatusCode statusCode;
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                var contentType = response.ContentType;
+                Encoding encoding = null;
+                if (contentType != null)
+                {
+                    var match = Regex.Match(contentType, @"(?<=charset\=).*");
+                    if (match.Success)
+                        encoding = Encoding.GetEncoding(match.ToString());
+                }
+
+                encoding = encoding ?? Encoding.UTF8;
+
+                statusCode = ((HttpWebResponse)response).StatusCode;
+                using (var reader = new StreamReader(stream, encoding))
+                    content = reader.ReadToEnd();
+            }
+            return content;
+        }
+
+        public static string[] getHtmlSplitted(string text)
+        {
+            var list = new List<string>();
+            var pattern = "(<a|</a>)";
+            var isInTag = false;
+            var inTagValue = String.Empty;
+
+            foreach (var subStr in Regex.Split(text, pattern))
+            {
+                if (subStr.Equals("<a"))
+                {
+                    isInTag = true;
+                    continue;
+                }
+                else if (subStr.Equals("</a>"))
+                {
+                    isInTag = false;
+                    list.Add(inTagValue.Substring(inTagValue.LastIndexOf(">") + 1));
+                    continue;
+                }
+
+                if (isInTag)
+                {
+                    inTagValue = subStr;
+                    continue;
+                }
+
+                //list.Add(subStr);
+
+            }
+            list.RemoveAt(0);
+            return list.ToArray();
+        }
+
+    }
+}
